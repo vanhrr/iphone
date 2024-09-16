202ddd04ad9e73df298b2628d82218858f97a8e8
vanhrr <vietanh22122003@gmail.com>
2024-09-16 15:22:36 +0700

    3d model three js

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
+import Model from "./components/Model";
 import "./App.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Hero />
       <Highlights />
+      <Model />
     </main>
   );
 }
